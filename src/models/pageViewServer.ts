894d03ebb2b3799e2c8d6050fec9858c79da7961
ParkGeonRyul <qyskamslepf@gmail.com>
2024-03-15 17:21:42 +0900

    feature : pageview utils

--- src/models/pageViewServer.ts
+++ src/models/pageViewServer.ts
@@ -1,12 +1,14 @@
 import { model, Schema } from "mongoose";
 
+//메뉴바 번호 부여된 순서대로 함. 단, 9번은 메인페이지
 const pageViewsSchema = new Schema({
-  today: { type: Date, require: true },
-  totalView: { type: Date },
-  petView: { type: Date },
-  familyView: { type: Date },
-  childView: { type: Date },
-  sportView: { type: Date },
+  today: { type: String, require: true },
+  9: { type: Number },
+  1: { type: Number },
+  2: { type: Number },
+  3: { type: Number },
+  4: { type: Number },
+  5: { type: Number },
 });
 
 pageViewsSchema.index({ today: 1 }, { unique: true });
